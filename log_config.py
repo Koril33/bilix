52ab783e3eaf66b2de432cd20f3d5af4116ca404
dingjinghui <dingjinghui33@163.com>
2025-04-29 17:02:06 +0800

    project init

---
+++ log_config.py
@@ -0,0 +1,29 @@
+log_config_dict = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] - %(levelname)-8s :: %(message)s',
+        }
+    },
+    'handlers': {
+        'console_handler': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+            'formatter': 'default',
+            'level': 'DEBUG',
+        },
+    },
+    'loggers': {
+        f'bilix': {
+            'handlers': ['console_handler'],
+            'level': 'DEBUG',
+            'propagate': False,
+        }
+    },
+    'root': {
+        'handlers': ['console_handler'],
+        'level': 'WARNING',
+    }
+}
+
