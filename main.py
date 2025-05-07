93b77ea293db611814879aa97b7caab72c29aec2
dingjinghui <dingjinghui33@163.com>
2025-05-07 18:00:52 +0800

    fix: 删除 download_headers 的 Cookie 字段

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ from tool import load_urls_from_file, clean_bili_url
 app = typer.Typer()
 
 download_headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0',
-    # 'Cookie': "buvid3=540B6FB2-1B27-D498-A1F7-10907A412CB986733infoc; b_nut=1744025886; _uuid=C5D104C28-ACBF-DF810-DF91-8101610677210AE87578infoc; buvid4=BA1213FD-FE34-2E73-8B05-7E3BB50BD20C87758-025040711-CGSA5pzOeV9DsZE7bM%2BBfg%3D%3D; rpdid=|(YYYYlm~Rl0J'u~RR|kl))); bmg_af_switch=1; bmg_src_def_domain=i0.hdslb.com; header_theme_version=CLOSE; enable_web_push=DISABLE; enable_feed_channel=ENABLE; home_feed_column=5; share_source_origin=COPY; timeMachine=0; bsource=search_bing; fingerprint=97770f12afdfa9faac377b5c807d3cf1; buvid_fp_plain=undefined; buvid_fp=97770f12afdfa9faac377b5c807d3cf1; bp_t_offset_3305808=1057860800170950656; SESSDATA=3cb308bf%2C1760791532%2C48fff%2A41CjBTMeWaVU4f3fMPe7eG7J8QrQlAHWZJrx_Uoj_HmlkeQUwiy2-aJSMY8oXK6kbfaRkSVlBsRWJ0THVxRjBDOVlQSU41a3F2bEVxRlhhSmdGQktGYzRfZy03UnllNUZ3a180MjJiUDdCZnVwdmNwUE9CbDE0LVFTZTlyVDBPV0U0ekIyd3NBblhRIIEC; bili_jct=131de20e14a0a2796550bdc95ec13640; DedeUserID=384605143; DedeUserID__ckMd5=c089376be03f8134; VIP_DEFINITION_GUIDE=1; sid=7g7fnd8u; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDYxMDExNTgsImlhdCI6MTc0NTg0MTg5OCwicGx0IjotMX0.fg5s3uXvEcONxAgLKCLpuyk3BUT0yOU3bajsp97Dmj8; bili_ticket_expires=1746101098; browser_resolution=1672-838; VIP_CONTENT_REMIND=1; CURRENT_QUALITY=0; b_lsid=E416A7A7_19689693B8E; bp_t_offset_384605143=1061820510909759488; CURRENT_FNVAL=4048"
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0',
 }
 
 class BiliTask:
